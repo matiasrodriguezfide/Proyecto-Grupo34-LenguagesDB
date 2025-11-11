409d369eaa2942c88155e464426c2e19abb2f126
matiasrodriguezfide <mrodriguez70089@ufide.ac.cr>
2025-11-10 21:29:04 -0600

    Update test_connection.py

--- python/test_connection.py
+++ python/test_connection.py
@@ -1,1 +1,12 @@
+import oracledb
 
+try:
+    conn = oracledb.connect(
+        user="coffeewild",
+        password="wellwellwell",
+        dsn="localhost:1521/XEPDB1"
+    )
+    print("Connexion exitosa")
+    conn.close()
+except Exception as e:
+    print("Error:", e)
